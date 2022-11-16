544b0a76d9611e795b84054133efa3e4da7bd1e4
lelonov23 <stepan.leonov777@gmail.com>
2022-11-16 14:49:27 +0300

    init

---
+++ src/com/example/calculator/Main.java
@@ -0,0 +1,19 @@
+package com.example.calculator;
+
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        try (FileInputStream in = new FileInputStream("input.txt");
+        FileOutputStream out = new FileOutputStream("output.txt")) {
+            byte[] buffer = new byte[in.available()];
+            in.read(buffer, 0, buffer.length);
+            out.write(buffer, 0, buffer.length);
+        } catch (FileNotFoundException ex) {
+            System.out.println(ex.getMessage());
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+}
